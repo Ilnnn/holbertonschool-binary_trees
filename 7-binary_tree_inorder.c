b7e4333e5a3b91aecd3dabd1420210620c87f45e
Milaa-au <mila.audu@icloud.com>
2025-12-08 13:13:52 +0000

    Adding a script that traverses a binary tree using infix traversal.

---
+++ 7-binary_tree_inorder.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "binary_trees.h"
+
+/**
+ * binary_tree_inorder - Goes through a binary tree using in-order traversal
+ * @tree: Pointer to the root node of the tree to traverse
+ * @func: Pointer to a function to call for each node
+ *
+ * Description: Visits the left subtree, then the current node, then the right
+ * subtree. The function @func is called for each node's value.
+ */
+
+void binary_tree_inorder(const binary_tree_t *tree, void (*func)(int))
+{
+	if (tree == NULL || func == NULL)
+	{
+		return;
+	}
+
+	binary_tree_inorder(tree->left, func);
+	func(tree->n);
+	binary_tree_inorder(tree->right, func);
+}
