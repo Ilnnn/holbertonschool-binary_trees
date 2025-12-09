8a8582d2a1e420de3876177bd35dce4c91391111
Milaa-au <mila.audu@icloud.com>
2025-12-09 13:44:41 +0000

    Correction Betty.

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -6,17 +6,21 @@
  * binary_tree_size - Measures the size of a binary tree
  * @tree: Pointer to the root node of the tree to measure
  *
- * Description: Computes the total number of nodes in a binary tree by
- * recursively summing the sizes of the left and right subtrees and adding one
- * for the current node.
+ * Description:
+ * The size is the total number of nodes in the binary tree.
+ * If the tree is NULL, the function returns 0.
+ *
+ * Return: The size of the tree
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-        return (0);
+	if (tree == NULL)
+	{
+		return (0);
+	}
 
-    return (binary_tree_size(tree->left) +
-            binary_tree_size(tree->right) +
-            1);
+	return (binary_tree_size(tree->left) +
+			binary_tree_size(tree->right) +
+			1);
 }
